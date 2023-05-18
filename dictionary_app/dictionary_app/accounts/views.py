c78a2eedf606df56afab7f7ebc76335ecf779b7c
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-05-18 15:56:34 +0300

    create all needed accounts views

--- dictionary_app/dictionary_app/accounts/views.py
+++ dictionary_app/dictionary_app/accounts/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic as views
+from django.contrib.auth import views as auth_views, get_user_model, login
 
-# Create your views here.
+from dictionary_app.accounts.forms import UserCreateForm
+
+UserModel = get_user_model()
+
+
+class LoginView(auth_views.LoginView):
+    template_name = 'accounts/login.html'
+
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+
+
+class RegisterView(views.CreateView):
+    template_name = 'accounts/register.html'
+    form_class = UserCreateForm
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+        login(self.request, self.object)
+        return result
+
+
+class UserEditView(views.UpdateView):
+    template_name = 'accounts/user-edit.html'
+    model = UserModel
+    fields = ('first_name', 'last_name', 'email', 'picture')
+
+    def get_success_url(self):
+        return reverse_lazy('login')
+
+
+class UserDeleteView(views.DeleteView):
+    template_name = 'accounts/user-delete.html'
+    model = UserModel
+
+
+class SignOutView(auth_views.LogoutView):
+    next_page = reverse_lazy('login')
+    
+
+
