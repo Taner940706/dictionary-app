47deef95c02c48c2313b1d99a7c25ecc4029af08
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-05-20 15:13:00 +0300

    add dictionary API to home logic

--- dictionary_app/dictionary_app/dictionary/views.py
+++ dictionary_app/dictionary_app/dictionary/views.py
@@ -1,14 +1,33 @@
+import json
+import urllib.request
+
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 from django.views import generic as views
 
 
 @login_required
-class Home(views.ListView):
+class Home(views.TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        word = str(self.request.GET.get('city'))
+        source = urllib.request.urlopen('https://api.dictionaryapi.dev/api/v2/entries/en/' + word).read()
+        data = json.loads(source)
+
+        context['word'] = word
+        context['phonetics_text'] = str(data['phonetics']['text'])
+        context['phonetics_audio'] = str(data['phonetics']['audio'])
+        context['origin'] = str(data['origin'])
+
+        for i in range(3):
+            speech = str(data['meanings'][i]['partOfSpeech'])
+            context['partOfSpeech' + speech] = str(data['meanings'][i]['partOfSpeech'])
+            context['definition' + speech] = str(data['meanings'][i]['definitions']['definition'])
+            context['example' + speech] = str(data['meanings'][i]['definitions']['example'])
+            context['synonyms' + speech] = str(data['meanings'][i]['definitions']['synonyms'])
+            context['antonyms' + speech] = str(data['meanings'][i]['definitions']['antonyms'])
+
         return context
 
 
