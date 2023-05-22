0d8f37547f4644bf2ac4b98735329d23b9f36f35
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-05-22 15:53:01 +0300

    add admin to the account app

--- dictionary_app/dictionary_app/accounts/admin.py
+++ dictionary_app/dictionary_app/accounts/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin, get_user_model
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+from dictionary_app.accounts.forms import UserEditForm
 
 # Register your models here.
+UserModel = get_user_model()
+
+
+@admin.register(UserModel)
+class UserAdmin(auth_admin.UserAdmin):
+    form = UserEditForm
+    add_form = UserCreationForm
+
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'username',
+                    'password',
+                ),
+            }),
+        (
+            'Personal Info',
+         {
+             'fields': (
+                 'first_name',
+                 'last_name',
+                 'email',
+                 'picture',
+             ),
+         },
+    ),
+    (
+        'Permissons',
+        {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            ),
+        },
+    ),
+    (
+        'Important dates',
+        {
+            'fields': (
+                'last_login',
+                'date_joined',
+            ),
+        },
+    ),
+    )
