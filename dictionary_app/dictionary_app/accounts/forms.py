d8c8f82a74571866575e5bbb845def9df13e07f6
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-05-27 17:10:43 +0300

    create html structure for nav, login, register, delete user and edit user

--- dictionary_app/dictionary_app/accounts/forms.py
+++ dictionary_app/dictionary_app/accounts/forms.py
@@ -5,11 +5,13 @@ UserModel = get_user_model()
 
 class UserEditForm(auth_forms.UserChangeForm):
     class Meta:
+        model = UserModel
         fields = '__all__'
         field_classes = {'username': auth_forms.UsernameField, }
 
 
 class UserCreateForm(auth_forms.UserCreationForm):
     class Meta:
+        model = UserModel
         fields = ('username', 'email', "password1", "password2")
         field_classes = {'username': auth_forms.UsernameField, }
--- dictionary_app/dictionary_app/urls.py
+++ dictionary_app/dictionary_app/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from dictionary_app.dictionary.views import View404, View500
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('dictionary_app.dictionary.urls')),
     path('', include('dictionary_app.accounts.urls')),
 ]
 
-handler404 = 'dictionary_app.dictionary_app.View404'
-handler500 = 'dictionary_app.dictionary_app.View500'
+handler404 = View404.as_view()
+handler500 = View500.as_view()
