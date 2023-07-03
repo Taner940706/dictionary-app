b7af101c726d38b51a1b2c6cf6b5ed71d0007450
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-07-03 20:46:42 +0300

    start to create home.html with views

--- dictionary_app/dictionary_app/dictionary/views.py
+++ dictionary_app/dictionary_app/dictionary/views.py
@@ -12,9 +12,17 @@ class HomeView(views.TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
+
+        part_of_speech = []
+        definiton = []
+        example = []
+        synonyms = []
+        antonyms = []
+
+
         context = super().get_context_data(**kwargs)
 
-        word = str(self.request.GET.get('city'))
+        word = str(self.request.GET.get('word'))
 
         source_dict = urllib.request.urlopen('https://api.dictionaryapi.dev/api/v2/entries/en/' + word).read()
         source_pic = urllib.request.urlopen('https://pixabay.com/api/?key=' + config('API_KEY_PIC') + '&q=' + (word.replace(" ", "%20"))).read()
@@ -25,15 +33,15 @@ class HomeView(views.TemplateView):
         context['word'] = word
         context['phonetics_text'] = str(data_dict['phonetics']['text'])
         context['phonetics_audio'] = str(data_dict['phonetics']['audio'])
+        context['source'] = str(data_dict['phonetics']['sourceUrl'])
         context['origin'] = str(data_dict['origin'])
 
         for i in range(3):
-            speech = str(data_dict['meanings'][i]['partOfSpeech'])
-            context['partOfSpeech' + speech] = str(data_dict['meanings'][i]['partOfSpeech'])
-            context['definition' + speech] = str(data_dict['meanings'][i]['definitions']['definition'])
-            context['example' + speech] = str(data_dict['meanings'][i]['definitions']['example'])
-            context['synonyms' + speech] = str(data_dict['meanings'][i]['definitions']['synonyms'])
-            context['antonyms' + speech] = str(data_dict['meanings'][i]['definitions']['antonyms'])
+            part_of_speech.append(str(data_dict['meanings'][i]['partOfSpeech']))
+            definiton.append(str(data_dict['meanings'][i]['definitions']['definition']))
+            example.append(str(data_dict['meanings'][i]['definitions']['example']))
+            synonyms.append(str(data_dict['meanings'][i]['definitions']['synonyms']))
+            antonyms.append(str(data_dict['meanings'][i]['definitions']['antonyms']))
 
         context['picture'] = data_pic['hits'][random.randint(0, len(data_pic['hits']))]['webformatURL']
 
